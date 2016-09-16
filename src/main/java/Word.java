2c5c852f2fb901333c4c7b30d14867009f2859a5
Ewa Manek <manekej@gmail.com>
2016-09-16 09:48:42 -0700

    Finish Word object methods and tests

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -1,13 +1,18 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class Word {
-  String mWord;
-  List<Definition> mDefinitions;
+  private String mWord;
+  private List<Definition> mDefinitions;
+  private int mId;
+  private static List<Word> instances = new ArrayList<Word>();
 
   public Word(String word){
     mWord = word;
     mDefinitions = new ArrayList<Definition>();
+    instances.add(this);
+    mId=instances.size();
   }
 
   public String getWord(){
@@ -18,7 +23,43 @@ public class Word {
     return mDefinitions;
   }
 
+  public int getId(){
+    return mId;
+  }
+
   public void addDefinition(Definition definition){
     mDefinitions.add(definition);
+  }
+
+  public boolean removeDefinition(String defString){
+    for(Definition definition: mDefinitions){
+      if(definition.getDefString().equals(defString)){
+        return mDefinitions.remove(definition);
+      }
+    }
+    return false;
+  }
+
+  public List<Definition> searchDefinitions(String searchString){
+    List<Definition> foundDefs = mDefinitions.stream().filter(definition -> (definition.getDefString().toLowerCase().contains(searchString))).collect(Collectors.toList());
+    return foundDefs;
+  }
+
+  public static List<Word> all(){
+    return instances;
+  }
+
+  public static void clear(){
+    instances.clear();
+  }
+
+  public static Word find(int id){
+    return instances.get(id-1);
+  }
+
+  public static List<Word> findByDef(String searchString){
+    List<Word> foundWords = null;
+    foundWords = instances.stream().filter(instance -> (instance.searchDefinitions(searchString).size() > 0)).collect(Collectors.toList());
+    return foundWords;
   }
 }
--- src/test/java/WordTest.java
+++ src/test/java/WordTest.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class WordTest{
+  @After
+  public void tearDown() {
+    Word.clear();
+  }
 
   @Test
   public void Word_instantiatesCorrectly() {
@@ -32,10 +36,96 @@ public class WordTest{
   }
 
   @Test
-  public void getWords_instantiatesCorrectly() {
+  public void removeDefinition_doesNothingIfDefinitionNotFound_false() {
     Word word = new Word("word");
     Definition definition = new Definition("definition");
     word.addDefinition(definition);
-    assertTrue(word.getDefinitions().contains(definition));
+    assertTrue(!word.removeDefinition("definition2"));
+  }
+
+  @Test
+  public void removeDefinition_removesDefinitionIfFound_true() {
+    Word word = new Word("word");
+    Definition definition = new Definition("definition");
+    word.addDefinition(definition);
+    assertTrue(word.removeDefinition("definition"));
+    assertEquals(0, word.getDefinitions().size());
+  }
+
+  @Test
+  public void findDefinition_returnsEmptyListIfNotFound_true() {
+    Word word = new Word("word");
+    Definition definition = new Definition("definition");
+    word.addDefinition(definition);
+    assertEquals(0, word.searchDefinitions("pants").size());
+  }
+
+  @Test
+  public void findDefinition_findsMatchingDefinitionsInAWord_true() {
+    Word word = new Word("word");
+    Definition definition = new Definition("some definition");
+    word.addDefinition(definition);
+    assertTrue(word.searchDefinitions("definition").contains(definition));
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfword_true() {
+    Word word = new Word("word");
+    Word word2 = new Word("word2");
+    assertTrue(Word.all().contains(word));
+    assertTrue(Word.all().contains(word2));
+  }
+
+  @Test
+  public void clear_removesAllWordsFromList_0() {
+    Word word = new Word("word");
+    Word word2 = new Word("word2");
+    Word.clear();
+    assertEquals(0, Word.all().size());
+  }
+
+  @Test
+  public void getId_returnsId_int() {
+    Word word = new Word("word");
+    assertEquals(1, word.getId());
+  }
+
+  @Test
+  public void find_returnsCorrectWord_Word(){
+    Word word = new Word("word");
+    Word word2 = new Word("word2");
+    assertEquals(word2, Word.find(word2.getId()));
+  }
+
+  @Test
+  public void findByDef_returnsCorrectWord_true(){
+    Word word = new Word("word");
+    Definition definition = new Definition("definition");
+    word.addDefinition(definition);
+    assertTrue(Word.findByDef("definition").contains(word));
+  }
+
+  @Test
+  public void findByDef_returnsAllCorrectWords_true(){
+    Word word = new Word("word");
+    Definition definition = new Definition("definition");
+    word.addDefinition(definition);
+    Word word2 = new Word("word2");
+    Definition definition2 = new Definition("also definition");
+    word2.addDefinition(definition2);
+    assertTrue(Word.findByDef("definition").contains(word));
+    assertTrue(Word.findByDef("definition").contains(word2));
+  }
+
+  @Test
+  public void findByDef_doesNotReturnIncorrectWords_true(){
+    Word word = new Word("word");
+    Definition definition = new Definition("definition");
+    word.addDefinition(definition);
+    Word word2 = new Word("word2");
+    Definition definition2 = new Definition("something else");
+    word2.addDefinition(definition2);
+    assertTrue(Word.findByDef("definition").size() > 0);
+    assertTrue(!Word.findByDef("definition").contains(word2));
   }
 }
